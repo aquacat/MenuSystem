ebb7f90f45d61ce579e2dc7814a13c3ce30a459e
Noah Anderson <andernoha@gmail.com>
2018-11-30 15:40:07 -0600

    Initial commit

---
+++ Assets/Scenes/Scripts/SceneSwitch.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneSwitch : MonoBehaviour {
+	public void SceneSwitcher (int idx){
+		SceneManager.LoadScene (idx);
+	}
+
+}
